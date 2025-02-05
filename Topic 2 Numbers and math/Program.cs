1bad4ad3b85ec409594c3c7eedf0fadcd2d06702
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-05 14:17:19 -0500

    Finished the Lesson.

--- Topic 2 Numbers and math/Program.cs
+++ Topic 2 Numbers and math/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Here is the amount of money you would make if you earned $21/hr for 8 hours:");
+            Console.WriteLine(21 * 8);
+            Console.WriteLine("");
+            Console.WriteLine("Here is the amount it would cost to get a dozen eggs:");
+            Console.WriteLine(6 * 12);
+            Console.WriteLine("");
+            Console.WriteLine("The tax on $79.99 is " + (79.88 * 0.13).ToString("C"));
+            Console.WriteLine("");
+            Console.WriteLine("There are " + (5 * 12 + 6) * 2.54 + " centimeters in 5'6\"");
         }
     }
 }
